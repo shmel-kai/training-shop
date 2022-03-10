c670e37200fd01efcfb49bb9f09333220e13b661
Alexandra <al88.korotkevich@gmail.com>
2022-03-10 16:16:38 +0300

    fix: fix items block filtering

--- src/components/ItemsBlock/ItemsBlock.js
+++ src/components/ItemsBlock/ItemsBlock.js
@@ -1,6 +1,7 @@
-import './styles.scss'
 import { Card } from './Card/index';
 import { useState } from 'react';
+
+import './styles.scss';
 
 const ARRIVALS_FLAG = 'isNewArrivals';
 const SPECIAL_FLAG = 'isSpecial';
@@ -9,34 +10,15 @@ const VIEWED_FLAG = 'isMostViewed';
 const FEATURED_FLAG = 'isFeatured';
 
 const ItemsBlock = ({ products, title, headerBlock = true, buttonAll = true, type }) => {
-    const [filters, setFilters] = useState([]);
-
-    const onItemsListClick = (tag) => {
-        if (!filters.includes(tag)) {
-            setFilters([...filters, tag]);
-        } else {
-            const tempArr = [...filters];
-            const index = tempArr.indexOf(tag);
-            tempArr.splice(index, 1);
-            setFilters(tempArr);
-        }
-    };
+    const [currentItem, setCurrentItem] = useState(null);
 
     const filteredArray = products
-            .filter((item) => {
-            let isProductFits = false;
-
-            if (filters.length) {
-                filters.forEach((filter) => {
-                    if (item.particulars[filter]) {
-                        isProductFits = true;
-                    }
-                });
-            } else {
-                isProductFits = true;
+        .filter((product) => {
+            if (currentItem) {
+                return product.particulars[currentItem]
             }
 
-            return isProductFits;
+            return true;
         })
         .splice(0, 8);
 
@@ -51,19 +33,34 @@ const ItemsBlock = ({ products, title, headerBlock = true, buttonAll = true, typ
                         </div>
                         <nav className='ItemsBlock-nav'>
                             <ul className='ItemsBlock-list'>
-                                <li onClick={() => onItemsListClick(ARRIVALS_FLAG)}>
+                                <li 
+                                    className={currentItem === ARRIVALS_FLAG ? 'active' : ''}
+                                    onClick={() => setCurrentItem(ARRIVALS_FLAG)}
+                                >
                                     NEW ARRIVALS
                                 </li>
-                                <li onClick={() => onItemsListClick(SPECIAL_FLAG)}>
+                                <li 
+                                    className={currentItem === SPECIAL_FLAG ? 'active' : ''}
+                                    onClick={() => setCurrentItem(SPECIAL_FLAG)}
+                                >
                                     SPECIALS
                                 </li>
-                                <li onClick={() => onItemsListClick(BESTSELLER_FLAG)}>
+                                <li 
+                                    className={currentItem === BESTSELLER_FLAG ? 'active' : ''}
+                                    onClick={() => setCurrentItem(BESTSELLER_FLAG)}
+                                >
                                     BESTSELLERS
                                 </li>
-                                <li onClick={() => onItemsListClick(VIEWED_FLAG)}>
+                                <li 
+                                    className={currentItem === VIEWED_FLAG ? 'active' : ''}
+                                    onClick={() => setCurrentItem(VIEWED_FLAG)}
+                                >
                                     MOST VIEWED
                                 </li>
-                                <li onClick={() => onItemsListClick(FEATURED_FLAG)}>
+                                <li 
+                                    className={currentItem === FEATURED_FLAG ? 'active' : ''}
+                                    onClick={() => setCurrentItem(FEATURED_FLAG)}
+                                >
                                     FEATURED PRODUCTS
                                 </li>
                             </ul>
